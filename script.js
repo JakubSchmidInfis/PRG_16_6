541c300e80c09d0c8bc1f11a34a910b68eeed7c9
unknown <jakub.schmid@infis.cz>
2025-06-16 12:25:13 +0200

    skibidiiiiiiiioiiiiiiiiiiiiiiiiiiii

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const answerDiv = document.getElementById("answerDiv")
 const questionDiv = document.getElementById("questionDiv")
 const nextButton = document.getElementById("button")
 
+const json = [
+{
+    "question": "Kolik je 1 + 1? ; 2,5,4,3,5,6"
+},
+{
+    "question": "Jaký je smysl života? ; Skibidi toilet, TikTok, Rainbow Siege (se)X, Helldivers II, I hate the Juice, Adolf Heatler"
+},
+{
+    "question": "Gedagedigedagedageooooo? ; Ajv Bín meríd lonk tajm agou,Pneumonoultramicroscopicislilcovolcanoconiosis",
+},
+{
+    "question": "Meatball! ; Who's ball?, Hi ball!",
+}
+]
+
+var garbage = []
+
 var question = 0;
+var maxQuestions = json.length;
 
 function nextQuestion(){
-
+    Clear()
+    changeButton()
     question ++;
 
     //Assign
@@ -17,11 +36,13 @@ function nextQuestion(){
     answerText = answerText.trim();
     questionText = questionText.trim();
 
-    alert("Question: " + questionText + " Answer: " + answerText);
+    //Clear
+    
 
     //Přidat elementy do dokumentu
     writeQuestion(questionText);
     writeAnswer(answerText);
+
 }
 
 function writeQuestion(text){
@@ -29,7 +50,11 @@ function writeQuestion(text){
     questionItem.textContent = text
     questionItem.classList.add("question")
     questionDiv.append(questionItem)
+
+    garbage.push(questionItem)
 }
+
+
 function writeAnswer(text){
 
     let Answers = text.split(",")
@@ -37,29 +62,57 @@ function writeAnswer(text){
     for (i = 0;i<Answers.length;i++)
     {
         const answerItem = document.createElement("input")
+        const answerDivDiv = document.createElement("div")
         const answerLabel = document.createElement("label")
         
+        
+
+        answerDivDiv.id = "answerDivDiv"
+
+        //inputs and labels
         answerItem.id = ("answer" + String(i))
         answerLabel.htmlFor = ("answer" + String(i))
-
         answerItem.setAttribute("type", "radio")
         answerItem.setAttribute("name", "answer")
         answerLabel.textContent = Answers[i]
         answerItem.classList.add("answer")
-        answerDiv.append(answerItem)
-        answerDiv.append(answerLabel)
+
+        //appending
+        answerDiv.append(answerDivDiv)
+        answerDivDiv.append(answerItem)
+        answerDivDiv.append(answerLabel)
+
+        garbage.push(answerDivDiv)
+        garbage.push(answerItem)
+        garbage.push(answerLabel)
     }
-    
-
-
-
-    const answerItem = document.createElement("h3")
-    answerItem.textContent = text
-    answerItem.classList.add("answer")
-    answerDiv.append(answerItem)
 }
+
+function changeButton(){
+    if(question < maxQuestions)
+    {
+       button.textContent = "Next" 
+    }
+    else{
+        button.textContent = "Finish"
+    }
+}
+
+function Clear(){
+    for (i=0;i<garbage.length;i++){
+        garbage[i].remove()
+        
+    }
+    garbage = []
+}
+
+    const quests = ["Kolik je 1 + 1? ; 2,5,4,3,5,6", "Kolik je 5 + 4? ; 9,5,4,3,5,6"]
 
 function chooseQuestion(){
     
-    return "Kolik je 1 + 1? ; 2,5,4,3,5,6 "
+
+    //let random = (ceil(Math.random() *10)-9 * quests.length)
+    return(json[Math.round(Math.random() * (json.length-1))]).question;
+    //Credit Motlíkovi za pomoc!!
 }
+
